66efcc8540befa68cc2841bd4cdcd0df2a15e8a4
KB Pablo <kbpabloqr@gmail.com>
2026-05-28 19:43:25 -0400

    Fix iOS light mode top status bar

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "242464670" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "388348909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
