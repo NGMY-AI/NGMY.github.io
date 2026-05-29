4f60bf989b8819a20dbabc7c444f113b7cd99ef7
KB Pablo <kbpabloqr@gmail.com>
2026-05-29 13:56:00 -0400

    Publish web app update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1618945664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "514566629" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
